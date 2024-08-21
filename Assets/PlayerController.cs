be3be9a4b5541f2a38d6f96dc60c25c03e542168
Vic <vic.trouble@gmail.com>
2024-08-21 22:58:11 +0300

    Mario turns into direction of moving

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     {
         var body = character_.GetComponent<Rigidbody2D>();
 
-        int direction = 0;
+        int direction = 1;
         if (Input.GetKey(KeyCode.RightArrow))
         {
             body.AddForce(new Vector2(SPEED * Time.deltaTime, 0));
@@ -24,6 +24,6 @@ public class PlayerController : MonoBehaviour
             direction = -1;
         }
 
-        //body.transform.localScale = new Vector3(direction, 1, 1);
+        body.transform.localScale = new Vector3(direction, 1, 1);
     }
 }
