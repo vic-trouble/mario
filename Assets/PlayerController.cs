75fc1b213001651e948fa9af7a1c0ca23eef4f26
Vic <vic.trouble@gmail.com>
2024-08-22 23:23:40 +0300

    Add jumping

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,14 +7,17 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject character_;
     public float SPEED = 300;
+    public float JUMP_HEIGHT = 300;
     public float RUN_SPEED = 300;
     public float WALK_SPEED = 100;
+    private bool jumpingAction_ = false;
 
     void FixedUpdate()
     {
         var body = character_.GetComponent<Rigidbody2D>();
 
-        int direction = 1;
+        // apply movement
+        int direction = 1;  // TODO: encapsulate in Unit class
         if (Input.GetKey(KeyCode.RightArrow))
         {
             body.AddForce(new Vector2(SPEED * Time.deltaTime, 0));
@@ -27,8 +30,10 @@ public class PlayerController : MonoBehaviour
             direction = -1;
         }
 
+        // turn to the side of movement
         body.transform.localScale = new Vector3(direction, 1, 1);
 
+        // animate
         var animator = character_.GetComponent<Animator>();
         float speed = Mathf.Abs(body.velocity[0]);
         if (speed >= RUN_SPEED)
@@ -45,6 +50,25 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("walk", false);
             animator.SetBool("run", false);
+        }
+    }
+
+    void Update()
+    {
+        var body = character_.GetComponent<Rigidbody2D>();
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (/*!isAirborne && */ !jumpingAction_)
+            {
+                jumpingAction_ = true;
+                body.AddForce(new Vector2(0, JUMP_HEIGHT));
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            jumpingAction_ = false;
         }
     }
 }
