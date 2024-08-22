711d24d5d6dab3fff8ab797f0c892dfdf16a3dcc
Vic <vic.trouble@gmail.com>
2024-08-22 23:17:19 +0300

    Animate walk/run

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject character_;
     public float SPEED = 300;
+    public float RUN_SPEED = 300;
+    public float WALK_SPEED = 100;
 
     void FixedUpdate()
     {
@@ -25,5 +28,23 @@ public class PlayerController : MonoBehaviour
         }
 
         body.transform.localScale = new Vector3(direction, 1, 1);
+
+        var animator = character_.GetComponent<Animator>();
+        float speed = Mathf.Abs(body.velocity[0]);
+        if (speed >= RUN_SPEED)
+        {
+            animator.SetBool("walk", false);
+            animator.SetBool("run", true);
+        }
+        else if (speed >= WALK_SPEED)
+        {
+            animator.SetBool("walk", true);
+            animator.SetBool("run", false);
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+            animator.SetBool("run", false);
+        }
     }
 }
